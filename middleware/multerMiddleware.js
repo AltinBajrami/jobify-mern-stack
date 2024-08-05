05eb32ccfc2f068320fa1e109c20f0524271de3f
AltinBajrami <altin.bajrami66@gmail.com>
2024-08-05 12:07:21 +0200

    thir commit

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,5 +1,5 @@
 import multer from 'multer';
-import DataParser from 'datauri/parser.ts';
+import DataParser from 'datauri/parser.d.ts';
 import path from 'path';
 
 const storage = multer.memoryStorage();
